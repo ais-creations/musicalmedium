146c7c95dc325a48d21bb9008a869cb41269dd21
ssz18 <ssz18@ic.ac.uk>
2020-05-28 15:54:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Musical Medium inni.
-        </p>
+        <p>Musical Medium inni. Hi Im Aahil.</p>
       </header>
     </div>
   );
