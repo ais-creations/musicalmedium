fb16e65b46f95627551b01d320aecf6366b02a0f
Aahil <aahilmehta12@gmail.com>
2020-05-28 23:36:59 +0530

    Create project layout and tab pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from "./components/main";
+import {Link} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Musical Medium inni. Hi Im Aahil.</p>
-      </header>
+    <div className="content">
+      <Layout>
+        <Header className="header" title="Pink Fluffy Unicorn" scroll>
+          <Navigation>
+            <Link to="/">Home</Link>
+            <Link to="/feed">Feed</Link>
+            <Link to="/gigs">Gigs</Link>
+            <Link to="/musicians">Musicians</Link>
+            <Link to="/profile">Profile</Link>
+          </Navigation>
+        </Header>
+        <Content>
+          <div className="page-content"/>
+          <p>Musical Medium</p>
+          <Main/>
+        </Content>
+      </Layout>
     </div>
   );
 }
