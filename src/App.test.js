ce206268990cd14dd30f64b66c75a468d2bf3d65
Aahil <aahilmehta12@gmail.com>
2020-06-04 22:28:27 +0530

    add simple test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('dummy test', () => {
-
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/inni/i);
+  expect(linkElement).toBeInTheDocument();
 });
 
