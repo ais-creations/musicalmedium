4c52a054c1b77d318df9a3c76150989d0c619e1d
Indip Niroula <indip.niroula18@imperial.ac.uk>
2020-06-04 15:55:25 +0100

    rollback to react skeleton

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,6 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('dummy test', () => {
-<<<<<<< HEAD
 
-=======
-  // const { getByText } = render(<App />);
-  // const testElement = getByText(/Musical Medium/i);
-  //
-  // expect(testElement).toBeInTheDocument();
->>>>>>> c445e4af000ed4cfc353904b2e73663c96676479
 });
 
