ce8d38fc02e659dbe1aa337cf5bc35e2159286ef
Aahil <aahilmehta12@gmail.com>
2020-06-05 16:32:58 +0530

    add simple login html

---
+++ src/components/Login.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+// import axios from "axios";
+
+class Login extends Component {
+  
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login
