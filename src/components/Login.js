3599e43c251b8b06c99086dc5b1d347b7c4b24c2
Aahil <aahilmehta12@gmail.com>
2020-06-05 16:38:57 +0530

    fix render issues with login

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,30 +22,30 @@ class Login extends Component {
     });
   }
 
-  // handleSubmit(event) {
-  //   const { email, password } = this.state;
-  //
-  //   axios
-  //     .post(
-  //       "http://localhost:3001/sessions",
-  //       {
-  //         user: {
-  //           email: email,
-  //           password: password
-  //         }
-  //       },
-  //       { withCredentials: true }
-  //     )
-  //     .then(response => {
-  //       if (response.data.logged_in) {
-  //         this.props.handleSuccessfulAuth(response.data);
-  //       }
-  //     })
-  //     .catch(error => {
-  //       console.log("login error", error);
-  //     });
-  //   event.preventDefault();
-  // }
+  handleSubmit(event) {
+    const { email, password } = this.state;
+
+    // axios
+    //   .post(
+    //     "http://localhost:3001/sessions",
+    //     {
+    //       user: {
+    //         email: email,
+    //         password: password
+    //       }
+    //     },
+    //     { withCredentials: true }
+    //   )
+    //   .then(response => {
+    //     if (response.data.logged_in) {
+    //       this.props.handleSuccessfulAuth(response.data);
+    //     }
+    //   })
+    //   .catch(error => {
+    //     console.log("login error", error);
+    //   });
+    // event.preventDefault();
+  }
 
   render() {
     return (
