2fcb711b212fd5f8d6d02771443670461e023cf5
Indip Niroula <indip.niroula18@imperial.ac.uk>
2020-06-15 02:30:29 +0100

    add code to seperate production environment from developer environment

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ connection.once('open', () => {
   console.log("MongoDB connection established");
 })
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-})
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    })
+}
 
 // Passport middleware
 app.use(passport.initialize());
